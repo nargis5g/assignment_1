b679548a9c53081227ebcc5f8b13f5b7ef1167a8
Nargis <nargisjannat49@gmail.com>
2024-07-08 12:46:18 +0600

    assignment-1

---
+++ celcious_to_farenhit.php
@@ -0,0 +1,6 @@
+<?php
+$celsius = 32;
+$fahrenheit = (($celsius*9)/5)+32 ;
+echo("Temperature in Fahrenheit is: ");
+echo($fahrenheit);
+?>
---
+++ largest_number.php
@@ -0,0 +1,17 @@
+<?php
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if($num1>$num2){
+  echo "num1 is large";
+}
+else if($num1>$num3){
+    echo "num1 is large";
+}
+else if($num2>$num3){
+    echo "num2 is large";
+}
+else{
+    echo "num3 is large";
+}
